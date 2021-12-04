3ff8163a81865638dcd2161faef7535ddb5377b8
ashleylovette <ashleylovette15@gmail.com>
2021-12-03 23:48:52 -0600

    authentication cont.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Subject, throwError } from "rxjs";
+
+import { catchError, tap } from "rxjs/operators";
+import { User } from "./user.model";
+import { HTTPService } from "../shared/http.service";
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+
+@Injectable({ providedIn: 'root' })
+
+export class AuthService {
+  user = new Subject<User>();
+
+  constructor( private http: HttpClient, private httpService: HTTPService) {}
+
+  signup( email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.httpService.apiKey,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+      ).pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication(
+          resData.email,
+          resData.localId,
+          resData.idToken,
+          +resData.expiresIn);
+      }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.httpService.apiKey,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+      ).pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication(
+          resData.email,
+          resData.localId,
+          resData.idToken,
+          +resData.expiresIn);
+      }));
+    ;
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+    ) {
+    const expirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured!';
+    if(!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email is already associated with an account!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage="This account does not exist!";
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage="This password is not correct!";
+    }
+    return throwError(errorMessage);
+  }
+  }
+
+
